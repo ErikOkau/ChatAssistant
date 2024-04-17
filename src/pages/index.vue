87bdfc0d5ab499d40fe5e50165dd1f49f0fb9fc2
ErikOkau <erikmsorheim@yahoo.no>
2024-04-17 13:40:03 +0200

    set up tauri and nuxt. Everything works fine now

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { invoke } from '@tauri-apps/api'
+
+onMounted(async () => {
+    const retval = await invoke('greet')
+    console.log(retval)
+})
+</script>
+
+<template>
+    <h1>Hello Waifus!</h1>
+</template>
+
+<style scoped lang="scss">
+
+</style>
