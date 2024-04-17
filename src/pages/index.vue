977b40a39481b1846ea9b06300fbc7ef17c686fa
ErikOkau <erikmsorheim@yahoo.no>
2024-04-17 14:14:57 +0200

    tested printing a rust function to the page in index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { invoke } from '@tauri-apps/api'
 
+const message = ref('')
+
 onMounted(async () => {
-    const retval = await invoke('greet')
-    console.log(retval)
+    message.value = await invoke('greet')
 })
 </script>
 
 <template>
-    <h1>Hello Waifus!</h1>
+    <h1>Hello World!</h1>
+    <p>{{ message }}</p>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
