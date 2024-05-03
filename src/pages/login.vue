dbf87baf6f31083bb269cb31041ae67d82778547
ErikOkau <erikmsorheim@yahoo.no>
2024-05-03 11:19:29 +0200

    Add generic border radius variable and update login page layout

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -6,11 +6,18 @@ definePageMeta({
 </script>
 
 <template>
-    <div >
-        <h1>AAA</h1>
+    
+    <div class="row fit justify-center q-pt-lg">
+        <div class="text-white col-5 column items-center justify-center q-pb-xl">
+            <div class="text-h4">ChatAssistant</div>
+            <div class="text-subtitle1">Log in or create an account</div>
+        </div>
+        <div class="bg-white col-4 rounded-borders q-ma-xl" style="height: 471px;">
+            qfeq
+        </div>
     </div>
+    
 </template>
 
 <style scoped lang="scss">
-
 </style>
