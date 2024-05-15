207bb69a853363621d5747ce67db58d9d8f1eddd
ErikOkau <erikmsorheim@yahoo.no>
2024-05-15 11:32:50 +0200

    async is OP

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,27 +2,45 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::Manager;
+use mini_redis::client;
+use mini_redis::Result as RedisResult;
 use window_shadows::set_shadow;
 
-fn main() {
-    tauri::Builder::default()
-        // .invoke_handler(tauri::generate_handler![greet])
-        .setup(|app| {
 
+
+async fn say_world() {
+    println!("world!");
+}
+
+#[tokio::main]
+async fn main() -> RedisResult<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    client.set("hello", "world".into()).await?;
+    let redis_result = client.get("hello").await?;
+
+    let op = say_world();
+
+    let context = tauri::generate_context!();
+    let _app = tauri::Builder::default()
+        .setup(|app| {
             let window = app.get_window("main").unwrap();
             #[cfg(any(windows, target_os = "windows"))]
             set_shadow(&window, true).unwrap();
-
             Ok(())
         })
         .on_window_event(|event| match event.event() {
-          tauri::WindowEvent::CloseRequested { api, .. } => {
-            event.window().hide().unwrap();
-            api.prevent_close();
-          }
-          _ => {}
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+            _ => {}
         })
+        .run(context)?;
 
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        println!("got value from the server; result={:?}", redis_result);
+        println!("Hello");
+
+        op.await;
+
+    Ok(())
 }
