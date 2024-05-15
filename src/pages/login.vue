288bfb83d844a405633de400452c000d52be6c88
ErikOkau <erikmsorheim@yahoo.no>
2024-05-15 15:37:43 +0200

    changed some text

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -71,13 +71,12 @@ function clearInput() {
   <div class="row fit justify-center q-pt-lg">
     <div class="text-white col-5 column items-center justify-center q-pb-xl">
       <div class="text-h4">ChatAssistant</div>
-      <div class="text-subtitle1">Log in or create an account</div>
+      <div class="text-subtitle1" style="">Account created automatically upon login.</div>
     </div>
 
     <div class="bg-white col-4 rounded-borders q-ma-xl q-pa-xl" style="height: 471px;">
       <div class="">
         <div class="text-h5 text-weight-medium">Log in</div>
-        <div class="text-subtitle2">Not a user? <NuxtLink class="text-grey">Create an account</NuxtLink></div>
       </div>
 
       <div>
